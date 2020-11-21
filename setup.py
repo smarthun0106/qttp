a2055c23a32c85659ea36fd4fb8385629cb6eec8
HUNQUANT <gimtaehun@gimtaehuns-MacBook-Pro.local>
2020-11-21 13:09:16 +0900

    commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     # 배포할 패키지의 이름을 적어줍니다. setup.py파일을 가지는 폴더 이름과 동일하게 합니다.
     name                = 'qttp',
     # 배포할 패키지의 버전을 적어줍니다. 첫 등록이므로 0.1 또는 0.0.1을 사용합니다.
-    version             = '0.1',
+    version             = '0.2',
     # 배포할 패키지에 대한 설명을 작성합니다.
     description         = 'quant trading tools packages',
     # 배포하는 사람의 이름을 작성합니다.
