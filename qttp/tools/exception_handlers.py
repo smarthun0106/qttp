f17d65c04f786d3850e6ea8e3a8411332c62f25f
smarthun0106 <smarthun0106@gmail.com>
2020-12-02 17:30:50 +0900

    commita

--- qttp/tools/exception_handlers.py
+++ qttp/tools/exception_handlers.py
@@ -5,13 +5,13 @@ logger = setup_custom_logger("Apis")
 def exception_handler_01(func):
     def inner_function(*args, **kwargs):
         try:
-            result = func(*args, **kwargs)
+            return_value = func(*args, **kwargs)
             done_message = (
                 f"{args[0].api} - {args[0].market} - "
                 f"{func.__name__.upper()} Method Execute Well"
             )
             logger.info(done_message)
-            return result
+            return return_value
         except (ccxt.NetworkError, ccxt.InsufficientFunds, IndexError) as e:
             error_message = (
                         f"{args[0].api}, {args[0].market}, "
