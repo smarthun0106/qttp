f82a1093e1e21b578eb05b2ebf22f508384ef5d1
smarthun0106 <smarthun0106@gmail.com>
2020-12-12 10:59:15 +0900

    commita

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 setup(
     name                = 'qttp',
 
-    version             = '0.0.5 ',
+    version             = '0.0.6 ',
 
     description         = 'Quant Trading Tools Packages',
 
