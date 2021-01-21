1fa656a0cc10bd6ec3232248c222dd270bf1abb0
smarthun0106 <smarthun0106@gmail.com>
2021-01-22 08:36:05 +0900

    updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 setup(
     name                = 'qttp',
 
-    version             = '0.1.4',
+    version             = '0.1.5',
 
     description         = 'Quant Trading Tools Packages',
 
