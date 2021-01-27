2fa671122faeb859ca2eb1b9e264ed34ec24f71b
smarthun0106 <smarthun0106@gmail.com>
2021-01-27 19:59:49 +0900

    updated

--- qttp/tools/indicator.py
+++ qttp/tools/indicator.py
@@ -1,13 +1,32 @@
 import pandas as pd
+import numpy as np
 
-def bollinger_bands(f_df, ma=20, k=2):
+def bollinger_bands(df, ma=20, k=2):
     f_name = 'ma' + str(ma)
-    f_df[f_name] = f_df['close'].rolling(window=ma, center=False).mean()
-    f_df['std20'] = f_df['close'].rolling(window=ma).std(ddof=0)
-    f_df['bb_high'] = f_df[f_name] + f_df['std20']*k
-    f_df['bb_low'] = f_df[f_name] - f_df['std20']*k
-    f_df.dropna(inplace=True)
-    return f_df
+    df[f_name] = df['close'].rolling(window=ma, center=False).mean()
+    df['std20'] = df['close'].rolling(window=ma).std(ddof=0)
+    df['bb_high'] = df[f_name] + df['std20']*k
+    df['bb_low'] = df[f_name] - df['std20']*k
+    df['pb'] = (df['close']-df['bb_low']) / (df['bb_high']-df['bb_low'])
+    df['bbw'] = (df['bb_high']-df['bb_low']) / df[f_name]
+    return df
+
+def mfi(df, k):
+    # Money Flow Index
+    df['tp']  = (df['high'] + df['low'] + df['close']) / 3
+    df['pmf'] = 0
+    df['nmf'] = 0
+
+    for i in range(len(df['close'])-1):
+        if df['tp'].values[i] < df['tp'].values[i+1]:
+            df['pmf'].values[i+1] = df['tp'].values[i+1] * df['volume'].values[i+1]
+            df['nmf'].values[i+1] = 0
+        else:
+            df['nmf'].values[i+1] = df['tp'].values[i+1] * df['volume'].values[i+1]
+            df['pmf'].values[i+1] = 0
+    df['mfr'] = df['pmf'].rolling(window=k).sum() / df['nmf'].rolling(window=k).sum()
+    df['mfi' + str(k)] = 100 - 100 / (1 + df['mfr'])
+    return df
 
 def moving_average(df, ma_num):
     name = 'ma' + str(ma_num)
@@ -33,8 +52,8 @@ def rsi(df, feature, length):
     return df
 
 def fnMACD(m_Df, featrue, m_NumFast=12, m_NumSlow=26, m_NumSignal=9):
-    m_Df['EMAFast'] = m_Df[featrue].ewm( span = m_NumFast, min_periods = m_NumFast - 1).mean()
-    m_Df['EMASlow'] = m_Df[featrue].ewm( span = m_NumSlow, min_periods = m_NumSlow - 1).mean()
+    m_Df['EMAFast'] = m_Df[featrue].ewm(span=m_NumFast, min_periods=m_NumFast-1).mean()
+    m_Df['EMASlow'] = m_Df[featrue].ewm(span=m_NumSlow, min_periods=m_NumSlow-1).mean()
     m_Df['MACD'] = m_Df['EMAFast'] - m_Df['EMASlow']
     m_Df['MACDSignal'] = m_Df['MACD'].ewm( span = m_NumSignal, min_periods = m_NumSignal-1).mean()
     m_Df['MACDDiff'] = m_Df['MACD'] - m_Df['MACDSignal']
