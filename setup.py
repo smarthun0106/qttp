b5043b54f007bf3556c11c67caab5b7f91799d77
smarthun0106 <smarthun0106@example.com>
2021-09-29 20:38:42 +0900

    first commit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """ Upload Pip
 - python setup.py bdist_wheel
-- twine upload dist/qttp-0.4.3-py3-none-any.whl
+- twine upload dist/qttp-0.4.4-py3-none-any.whl
 wisebeggar / Luvfami****
 """
 
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
     name                = 'qttp',
 
-    version             = '0.4.3',
+    version             = '0.4.4',
 
     description         = 'Quant Trading Tools Packages',
 
