d2249f01be996c89cc251f018d078290884a1909
smarthun0106 <smarthun0106@example.com>
2022-03-23 11:42:36 +0900

    first commit

--- qttp/tools/hun_dropbox.py
+++ qttp/tools/hun_dropbox.py
@@ -1,28 +1,23 @@
 import dropbox
 import pandas as pd
 
-class Dropbox:
+class HunDropbox:
     def __init__(self, token):
         self.dbx = dropbox.Dropbox(token)
 
-    def get_file_list_names(self, path):
+    def get_file_list(self, path):
         contents = self.dbx.files_list_folder(path)
         files_name = [content.name for content in contents.entries]
         return files_name
 
-    def just_read_csv(self, path):
-        downfile, f = self.dbx.files_download(path)
-        df = pd.read_csv(f.raw)
-        return df
-
-    def read_csv(self, path):
-        downfile, f = self.dbx.files_download(path)
+    def read_csv(self, path, csv_name):
+        downfile, f = self.dbx.files_download(path+csv_name)
         df = pd.read_csv(f.raw, index_col=['date'])
         df.index = pd.to_datetime(df.index)
         return df
 
-    def create_file(self, path, dataframe):
-        df_string = dataframe.to_csv()
+    def save_csv(self, path, csv_name, df):
+        df_string = df.to_csv()
         db_bytes = bytes(df_string, 'utf8')
         self.dbx.files_upload(
             f=db_bytes,
