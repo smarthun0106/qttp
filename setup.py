8a54042d42924e7d5f5322c56af91600497559ed
smarthun0106 <smarthun0106@example.com>
2022-03-23 11:43:31 +0900

    first commit

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
 
     download_url        = 'https://github.com/smarthun0106/qttp/archive/master.zip',
 
-    install_requires    =  ['numpy', 'pandas', 'ccxt', 'dropbox'],
+    install_requires    =  ['numpy', 'pandas', 'dropbox'],
 
     packages            = find_packages(exclude = []),
 
